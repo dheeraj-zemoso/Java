0a8dfe6e72954603d1d5ec849684078e324b0146
EGIDELI VAMSHI KRISHNA <65390150+vamshi1159@users.noreply.github.com>
2022-05-09 18:41:43 +0530

    Update Assigment.java
    updated assignment

--- src/Assignments/Assigment.java
+++ src/Assignments/Assigment.java
@@ -9,34 +9,7 @@ class Assignment
         String s = "abBCDEDFGHIJKLMNOPqRSTUVWXYZ";
         System.out.println(betterCheck(s));
     }
-    private static boolean check(String s)
-    {
-        for (int i=0; i<26; i++)
-        {
-            char c = (char)('a'+i);
-            char C = (char)('A'+i);
-            int len = s.length();
-            boolean foundLowerCase = false;
-            boolean foundUpperCase = false;
-            for (int k=0; k<len; k++)
-            {
-                char x = s.charAt(k);
-                if (x==c)
-                {
-                    foundLowerCase = true;
-                }
-                if (x==C)
-                {
-                    foundUpperCase = true;
-                }
-            }
-            if (!foundLowerCase&&!foundUpperCase)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
+  
     private static boolean betterCheck(String s)
     {
         HashSet<Character> set = new HashSet<>();
